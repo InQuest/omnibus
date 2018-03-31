f9d928205999645058e7ce9650dc09f19e559bc8
deadbits <adam@deadbits.org>
2018-03-31 14:31:19 -0400

    fix import error

--- lib/modules/virustotal.py
+++ lib/modules/virustotal.py
@@ -8,14 +8,14 @@ import requests
 from common import is_ipv4
 from common import is_fqdn
 
-from common import get_apikeys
+from common import get_apikey
 
 
 def run_ip(ip):
     if not is_ipv4(ip):
         return None
 
-    vt_api = get_apikeys('virustotal')
+    vt_api = get_apikey('virustotal')
     url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
     parameters = {'ip': ip, 'apikey': vt_api}
     response = requests.get(url, params=parameters)
@@ -29,7 +29,7 @@ def run_host(domain):
     if not is_fqdn(domain):
         return None
 
-    vt_api = get_apikeys('virustotal')
+    vt_api = get_apikey('virustotal')
     url = 'https://www.virustotal.com/vtapi/v2/domain/report'
     parameters = {'domain': domain, 'apikey': vt_api}
     response = requests.get(url, params=parameters)
