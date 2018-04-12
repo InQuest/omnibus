f8cba4ebf8f9e24e436cc83dcec60957caffae0c
deadbits <adam@deadbits.org>
2018-04-12 19:22:22 -0400

    update sans module to use dshield library

--- lib/modules/sans.py
+++ lib/modules/sans.py
@@ -3,28 +3,19 @@
 # omnibus - deadbits.
 # query SANS ISC API
 ##
+import dshield
 from common import is_ipv4
-from common import http_get
 
 
 def run(host):
     if is_ipv4(host):
         result = None
-        url = 'https://isc.sans.edu/api/ip/%s?json' % host
-        headers = {'User-Agent': 'OSINT Omnibus (https://github.com/InQuest/Omnibus'}
 
         try:
-            status, response = http_get(url, headers=headers)
+            data = dshield.ip(host)
         except:
             return result
 
-        if status:
-            data = response.json()
-            result = {
-                'count': data['count']['text'],
-                'attacks': data['attacks']['text'],
-                'mindate': data['mindate']['text'],
-                'maxdate': data['maxdate']['text']
-            }
+        result = data['ip']
 
-        return result
+    return result
