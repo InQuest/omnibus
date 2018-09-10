d73c1e720f1d97aa104a4286187f785ef0dcaae5
deadbits <adam@deadbits.org>
2018-09-10 12:08:33 -0400

    cybercure module: grispan56

---
+++ lib/modules/cybercure.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+##
+# Developed by Alex (https://github.com/grispan56)
+# Module to implement indicators search functionality in cybercure.ai api
+#
+##
+from http import get
+
+from common import warning
+
+
+class Plugin(object):
+    def __init__(self, artifact):
+        self.artifact = artifact
+        self.artifact['data']['cybercure'] = None
+
+
+    def run(self):
+        url = 'http://api.cybercure.ai/feed/search?value=%s' % self.artifact['name']
+        headers = {'User-Agent': 'OSINT Omnibus (https://github.com/InQuest/Omnibus)'}
+
+        try:
+            status, response = get(url, headers=headers)
+            if status:
+                results = response.json()
+                self.artifact['data']['cybercure'] = results
+        except Exception as err:
+            warning('Caught exception in module (%s)' % str(err))
+
+
+def main(artifact):
+    plugin = Plugin(artifact)
+    plugin.run()
+    return plugin.artifact
+
--- omnibus-cli.py
+++ omnibus-cli.py
@@ -49,7 +49,7 @@ help_dict = {
         'new', 'cat', 'open', 'source', 'artifacts', 'delete'
     ],
     'modules': [
-        'blockchain', 'clearbit', 'censys', 'csirtg', 'cymon',
+        'blockchain', 'clearbit', 'censys', 'csirtg', 'csirtg', 'cymon',
         'dnsresolve', 'geoip', 'fullcontact', 'hackedemails', 'he', 'hibp',
         'ipinfo', 'ipvoid', 'isc', 'keybase', 'machine', 'nmap', 'passivetotal',
         'pgp', 'rss', 'shodan', 'threatcrowd',
@@ -399,6 +399,12 @@ class Console(cmd2.Cmd):
     def do_csirtg(self, arg):
         """Search CSIRTG for hash information"""
         result = self.dispatch.submit(self.session, 'csirtg', arg)
+        pp_json(result)
+
+
+    def do_cybercure(self, arg):
+        """Check if IP intelligence exists at cybercure.ai"""
+        result = self.dispatch.submit(self.session, 'cybercure', arg)
         pp_json(result)
 
 
